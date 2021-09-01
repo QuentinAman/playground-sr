c75d704abffa6d80b4d2f39170066734614b2b02
Quentin Aman Epitech <quentin.aman@epitech.eu>
2021-09-01 17:09:16 +0200

    DOOOOOOOOC bis

--- src/components/molecules/Divider/index.jsx
+++ src/components/molecules/Divider/index.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 
-export const Divider = ({ children, right, bottom, ...props }) => {
+/**
+ * Component divider
+ * @component
+ *
+ * @example
+ *  <Divider bottom right>
+        <Button>Button 1</Button>
+        <Button>Button 2</Button>
+    </Divider>
+ *
+ * @type {React.FC<{
+ *  className?: String,
+ *  children: React.ReactNode,
+ *  right?: String,
+ *  bottom?: String;
+ * }>}
+ */
+export const Divider = ({ children, className, right, bottom, ...props }) => {
     return (
-        <div className={`w-full flex ${right ? "flex-row-reverse" : ""} gap-3`}>
+        <div
+            {...props}
+            className={`${className || ''} w-full flex ${
+                right ? 'flex-row-reverse' : ''
+            } gap-3`}
+        >
             {children}
-            <div className={`border-solid w-full border-b self-${bottom ? "end" : "center"} border-black`}>
-            </div>
+            <div
+                className={`border-solid w-full border-b self-${
+                    bottom ? 'end' : 'center'
+                } border-black`}
+            ></div>
         </div>
     );
 };
